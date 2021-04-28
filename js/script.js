9dc25f267eaa9c8b35ef012af5571a4956849402
石川大地 <ishikawadaichi@daichinoMacBook-Air.local>
2021-04-28 19:10:12 +0900

    [Finish]PageTop_Lesson

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-$('.theTarget').skippr({
-  transiton: 'fade',
-  speed: 1000,
-  easing: 'easeOutQuart',
-  navType: 'block',
-  childrenElementType: 'div',
-  arrows: true,
-  autoPlay: true,
-  autoPlayDuration: 1000,
-  KeyboardOnAlways: true,
-  hidePrevious: false
-})
+$(function(){
+  $('#back a').on('click', function(event){
+    $('body, html').animate({
+      scrollTop: 0
+    }, 800);
+    event.preventDefault();
+  });
+});
